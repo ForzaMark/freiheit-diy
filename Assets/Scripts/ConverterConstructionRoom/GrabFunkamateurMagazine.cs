bbd75315510af676c355d5cd55443f221000195d
Mark Heimer <13_mark@protonmail.com>
2022-11-11 15:07:58 +0100

    implement grabing without script

--- Assets/Scripts/ConverterConstructionRoom/GrabFunkamateurMagazine.cs
+++ Assets/Scripts/ConverterConstructionRoom/GrabFunkamateurMagazine.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 public class GrabFunkamateurMagazine : XRGrabInteractable
 {
+    [SerializeField]
+    public GameObject RightHandAttachmentPoint;
+
     private Vector3 InteractorPosition = Vector3.zero;
 
     protected override void OnSelectEntered(XRBaseInteractor interactor)
     {
         base.OnSelectEntered(interactor);
         StoreInteractor(interactor);
-        MatchAttachmentPoints(interactor);
+        //MatchAttachmentPoints(interactor);
     }
 
     private void StoreInteractor(XRBaseInteractor interactor)
@@ -19,9 +22,16 @@ public class GrabFunkamateurMagazine : XRGrabInteractable
     private void MatchAttachmentPoints(XRBaseInteractor interactor)
     {
         bool hasAttached = attachTransform != null;
+        Debug.Log(attachTransform.transform.name);
         if (hasAttached)
         {
-            interactor.attachTransform.position = new Vector3(attachTransform.position.x + 1, attachTransform.position.y + 2, attachTransform.position.z - 5);
+            
+            interactor.transform.position = RightHandAttachmentPoint.transform.position;
+
+            Debug.Log(interactor.transform.position.x + " " + interactor.transform.position.y + " " + interactor.transform.position.z);
+            Debug.Log(RightHandAttachmentPoint.transform.position.x + " " + RightHandAttachmentPoint.transform.position.y + " " + RightHandAttachmentPoint.transform.position.z);
+            // interactor.attachTransform.position = new Vector3(RightHandAttachmentPoint.transform.position.x, RightHandAttachmentPoint.transform.position.y , RightHandAttachmentPoint.transform.position.z );
+            // interactor.attachTransform.position = new Vector3(attachTransform.position.x + 1, attachTransform.position.y + 2, attachTransform.position.z - 5);
         }
         else
         {
