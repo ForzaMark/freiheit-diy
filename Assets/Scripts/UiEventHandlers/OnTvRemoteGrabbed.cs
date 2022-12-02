1c5cd3404eec64a62404be2cb61a9d1cee074609
Mark Heimer <13_mark@protonmail.com>
2022-12-02 10:50:56 +0100

    refactoring

--- Assets/Scripts/UiEventHandlers/OnTvRemoteGrabbed.cs
+++ Assets/Scripts/UiEventHandlers/OnTvRemoteGrabbed.cs
@@ -1,10 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class openMediathek : MonoBehaviour
+public class OnTvRemoteGrabbed : MonoBehaviour
 {
-    public void OnGrab()
+    public void remoteGrabbed()
     {
         Application.Quit();
         Application.OpenURL("https://www.zdf.de/show/dalli-dalli/folge1-112.html");
